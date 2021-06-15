6814f04c0ebb8185bd14b5fec44d2fc1a8416afc
Leidy <paoalm1@hotmail.com>
2021-06-15 12:37:43 -0500

    definiendo si es un markdown

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
-module.exports = () => {
-  // ...
-};
+// const mdLinks = require("md-links");
+const fs = require("fs"); 
+const  jsdom  =  require ( "jsdom" ) ; 
+
+
+// comprobando que sea md 
+const path = './README.md';
+const extensionArchivo = /(.md)$/i;
+
+    if(!extensionArchivo.exec(path)){
+        console.log('no es un archivo md');
+        }
+		else { console.log("si es un archivo md")
+			// Leyendo el documento
+			fs.readFile(path, 'utf8' , (err, data) => {
+				if (err) {
+				  console.error(err)
+				  return
+				}
+				// imprimiendo docuemnto
+				console.log(data)
+			  })
+			}
+
+			 
+
+// mdLinks(path, object) 
+//   .then(links => {
+// 	  console.log(links)
+//     // => [{ href, text, file }]
+//   })
+
+
+
+// module.exports = () => {
+//   // ...
+// };
---
+++ modules/modules.js
@@ -0,0 +1,17 @@
+// const path = require('path');
+// // crear y leer archivos 
+// const fs = require('fs');
+// //  rutas y metodos para la extraccion de la informacion
+// const notes = '/users/joe/notes.txt'
+// path.dirname(notes) // /users/joe
+// path.basename(notes) // notes.txt
+// path.extname(notes) // .txt
+
+// stats.isFile() //true
+// stats.isDirectory() //false
+
+// const http = require('http') // metodos y status code 
+
+// const  {  JSDOM  }  =  jsdom ;
+// const dom = new JSDOM(`<!DOCTYPE html><p>Hello world</p>`);
+// console.log(dom.window.document.querySelector("p").textContent); // "Hello world"
